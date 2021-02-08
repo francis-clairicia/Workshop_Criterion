f373fdb05fb5908e43bc2cc500b966fbd0515fc3
Francis Clairicia-Rose-Claire-Josephine <fclairicia-rcj@frankysnow.fr>
2021-02-08 11:55:17 +0100

    [ADD] Tests with write ufnc

--- tests/test_write.c
+++ tests/test_write.c
@@ -21,3 +21,14 @@ Test(write, print_on_stdout)
     cr_assert_stdout_eq_str("Hello world");
     cr_assert_stderr_eq_str("Hello world");
 }
+
+Test(write, print_many_strings)
+{
+    char str[] = "Hello world\n";
+    char str2[] = "Welcome\n";
+
+    cr_redirect_stdout();
+    write(1, str, strlen(str));
+    write(1, str2, strlen(str2));
+    cr_assert_stdout_eq_str("Hello world\nWelcome\n");
+}
